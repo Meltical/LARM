f738f8b52b802eaff989ab1456181960a6e6ea25
MikUwU <victor.deliege@gmail.com>
2021-01-20 10:20:32 +0000

    publisher topic /bottle

--- src/vision/src/bottle_service_server.py
+++ src/vision/src/bottle_service_server.py
@@ -7,8 +7,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseActionFeedback
 
 def my_callback(request):
     global pose_result
-    print "[ BOTTLE ] Found at :"
-    print pose_result
+    pub.publish(pose_result)
     return EmptyResponse()
 
 def move_base_pose_callback(msg):
@@ -17,6 +16,7 @@ def move_base_pose_callback(msg):
 
 rospy.init_node('service_server') 
 my_service = rospy.Service('/bottle', Empty , my_callback)
+pub = rospy.Publisher('/bottle', Pose, queue_size=1)
 pose_result = Pose()
 pose_sub = rospy.Subscriber('/move_base/feedback', MoveBaseActionFeedback, move_base_pose_callback)
 rospy.spin()
